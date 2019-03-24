00d3cd4adcf8acf7a25fc759cb956076cc25058d
aQaTL <mmsoltys@outlook.com>
2019-03-24 16:22:02 +0100

    Browsing images

---
+++ frontend/browse_imgs.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const api_url = "/ai";
+
+const parseFilename = name =>
+	name === "" || name === null || name === undefined ? "unnamed" : name;
+
+window.onload = () => {
+	let app = new Vue({
+		el: '#app',
+		data: {
+			page: 0,
+			total_pages: 1,
+			pastes: [],
+		},
+		methods: {
+			infiniteHandler($state) {
+				if (this.page < this.total_pages) {
+					let req = new XMLHttpRequest();
+					req.open("GET", `${api_url}?page=${this.page + 1}`, true);
+					req.onload = () => {
+						if (req.status !== 200) {
+							$state.complete();
+							return;
+						}
+						let paginatedPastes = JSON.parse(req.responseText);
+						this.page = paginatedPastes.page;
+						this.total_pages = paginatedPastes.total_pages;
+						paginatedPastes.results
+							.forEach(paste => {
+								paste.filename = parseFilename(paste.filename);
+								this.pastes.push(paste);
+							});
+						$state.loaded();
+					};
+					req.send();
+				} else {
+					$state.complete();
+				}
+			},
+		}
+	});
+};
